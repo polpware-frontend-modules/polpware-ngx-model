61879c3061373faf06345a5b8c0b48df362fc2e9
Tom <xiaolong@cse.tamu.edu>
2019-01-28 11:15:56 -0500

    New publish

---
+++ bundles/polpware-ngx-model.umd.js
@@ -0,0 +1,11 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define('@polpware/ngx-model', factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+
+
+})));
+
+//# sourceMappingURL=polpware-ngx-model.umd.js.map
---
+++ esm2015/polpware-ngx-model.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export {} from './public_api';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicG9scHdhcmUtbmd4LW1vZGVsLmpzIiwic291cmNlUm9vdCI6Im5nOi8vQHBvbHB3YXJlL25neC1tb2RlbC8iLCJzb3VyY2VzIjpbInBvbHB3YXJlLW5neC1tb2RlbC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsZUFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljX2FwaSc7XG4iXX0=
